68e751423c41e9d71085cd5247d58d0ea1703789
vishal singh <vishalsingh@vishals-MacBook-Air-3.local>
2024-08-15 23:07:39 +0530

    Added

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,14 +5,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     rollupOptions: {
-//       external: ['react-router-dom'], // Add this line
-//     },
-//   },
-// });
