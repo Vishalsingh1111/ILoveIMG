1492aa999c00e1b3f90f602371760612187111f0
Vishal Kumar Singh <vishalsingh21575@gmail.com>
2024-10-23 23:52:38 +0530

    Update File.controller.js

--- Backend/Controller/File.controller.js
+++ Backend/Controller/File.controller.js
@@ -8,7 +8,7 @@ export const uploadImage = async (req, res) => {
     };
     try {
         const file = await File.create(fileObj);
-        res.status(200).json({ path: `http://localhost:8000/file/${file._id}` });
+        res.status(200).json({ path: `https://iloveimg-frontend.onrender.com/file/${file._id}` });
     } catch (error) {
         console.error(error.message);
         res.status(500).json({ error: error.message });
